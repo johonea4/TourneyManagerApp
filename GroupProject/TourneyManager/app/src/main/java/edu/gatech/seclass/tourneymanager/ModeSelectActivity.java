83f02ff3edf8ceab67868ed8379815bad5010e96
Joseph Honea <johonea@gatech.edu>
2017-02-25 12:34:38 -0600

    Added structure for App development

---
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/ModeSelectActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.tourneymanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ModeSelectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_select);
+    }
+}
