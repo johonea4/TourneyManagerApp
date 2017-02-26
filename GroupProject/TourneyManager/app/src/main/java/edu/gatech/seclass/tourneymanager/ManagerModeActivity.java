bfc6b8897a4a9471827f90bb1af5f5f737fe52d7
Joseph Honea <johonea@gatech.edu>
2017-02-25 23:01:42 -0600

    Added additional Activities

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/ManagerModeActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/ManagerModeActivity.java
@@ -1,8 +1,10 @@
 package edu.gatech.seclass.tourneymanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ManagerModeActivity extends Activity
 {
@@ -12,11 +14,77 @@ public class ManagerModeActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_mode);
 
-        appMode = ((TourneyManagerApp)getApplication()).getappMode();
+        m_app = (TourneyManagerApp)getApplication();
+        appMode = m_app.getappMode();
+        createButton = (Button)findViewById(R.id.createButton);
+        manageButton = (Button)findViewById(R.id.managePlayersButton);
+        playerTotalsButton = (Button)findViewById(R.id.playerTotalsButton);
+        tourneyTotalsButton = (Button)findViewById(R.id.TourneyTotalsButton);
+        roundsButton = (Button)findViewById(R.id.roundsButton);
+
     }
 
+    public void setButtons()
+    {
+        if(m_app.isTourneyRunning())
+        {
+            createButton.setText("End Tournament");
+            roundsButton.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            createButton.setText("Create Tournament");
+            roundsButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        setButtons();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        setButtons();
+    }
+
+    public void CreateTourneyLauncher(View view)
+    {
+        if(m_app.isTourneyRunning())
+        {
+            setButtons();
+        }
+        else
+        {
+            Intent createLauncher = new Intent(ManagerModeActivity.this, CreateTourney.class);
+            startActivity(createLauncher);
+        }
+    }
+    public void ManagePlayersLauncher(View view)
+    {
+
+    }
+    public void PlayerTotalsLauncher(View view)
+    {
+
+    }
+    public void TournamentTotalsLauncher(View view)
+    {
+
+    }
+    public void RoundsLauncher(View view)
+    {
+
+    }
 
     //Private members
-    AppMode appMode;
-
+    private TourneyManagerApp m_app;
+    private AppMode appMode;
+    private Button createButton;
+    private Button manageButton;
+    private Button playerTotalsButton;
+    private Button tourneyTotalsButton;
+    private Button roundsButton;
 }
--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/TourneyManagerApp.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/TourneyManagerApp.java
@@ -19,12 +19,31 @@ public class TourneyManagerApp extends Application
         SharedPreferences pref = getSharedPreferences("TourneyManagerPref", Context.MODE_PRIVATE);
         tourneyRunning = pref.getBoolean("tourneyRunning",tourneyRunning);
         pref.edit().putBoolean("tourneyRunning",tourneyRunning);
+        prefChanged=false;
     }
     public void setAppMode(AppMode m) { appMode = m; }
     public AppMode getappMode() { return appMode; }
-    public boolean isTourneyRunning() { return tourneyRunning; }
+    public boolean isTourneyRunning()
+    {
+        if(prefChanged)
+        {
+            prefChanged=false;
+            tourneyRunning = false;
+            SharedPreferences pref = getSharedPreferences("TourneyManagerPref", Context.MODE_PRIVATE);
+            tourneyRunning = pref.getBoolean("tourneyRunning", tourneyRunning);
+        }
+        return tourneyRunning;
+    }
+    public void setTourneyRunning(boolean r)
+    {
+        tourneyRunning=r;
+        SharedPreferences pref = getSharedPreferences("TourneyManagerPref", Context.MODE_PRIVATE);
+        pref.edit().putBoolean("tourneyRunning",tourneyRunning);
+        prefChanged = true;
+    }
 
     //Private Memebrs
     private AppMode appMode;
     private boolean tourneyRunning;
+    private boolean prefChanged;
 }
