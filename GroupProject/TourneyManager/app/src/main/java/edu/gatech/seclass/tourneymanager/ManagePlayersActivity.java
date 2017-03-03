e6f63ee29bee6dbaeb8e47697ba01171e94e01f2
Rajendra Ugrani <rgugrani@us.ibm.com>
2017-03-03 15:18:22 -0500

    raj-changes for db

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/ManagePlayersActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/ManagePlayersActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.List;
+
+import edu.gatech.seclass.tourneymanager.db.Player;
+import edu.gatech.seclass.tourneymanager.db.PlayerDBHelper;
+
 public class ManagePlayersActivity extends Activity {
 
     @Override
@@ -37,6 +42,14 @@ public class ManagePlayersActivity extends Activity {
     //TODO: Populate player list with stored players from Database
     public void populatePlayers()
     {
+        Player player = new Player();
+        player.setUserName("something from the screen");
+        player.setName("whatever");
+
+        PlayerDBHelper helper = new PlayerDBHelper(this);
+        helper.addPlayer(player);
+
+        List<Player> playerList = helper.getAllPlayers();
 
     }
 
