f56875904fade9f4a619bc0388dfdfa12509096e
Joseph Honea <johonea@gatech.edu>
2017-03-09 00:41:18 -0600

    Bug Fixes

--- GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/TourneyTotalsActivity.java
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/TourneyTotalsActivity.java
@@ -11,6 +11,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import edu.gatech.seclass.tourneymanager.Dao.TourneyManagerDao;
@@ -29,9 +31,22 @@ public class TourneyTotalsActivity extends Activity {
         numTourneys = (EditText)findViewById(R.id.numTourneyField);
         profit = (EditText)findViewById(R.id.profitField);
         refunded = (EditText)findViewById(R.id.refundedField);
+        tourneyIds = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tourneyIds);
+        tourneyList.setAdapter(adapter);
         setListCallBack();
     }
 
+    class TotalsCompare implements Comparator<Tournament>
+    {
+        @Override
+        public int compare(Tournament o1, Tournament o2) {
+            int t1 = o1.isEndedEarly() ? 0 : o1.getInfo().getHouseCut();
+            int t2 = o2.isEndedEarly() ? 0 : o2.getInfo().getHouseCut();
+
+            return t1 < t2 ? 1 : t1==t2 ? 0 : -1;
+        }
+    }
     //TODO: Populate Info
     @Override
     protected void onStart(){
@@ -39,27 +54,28 @@ public class TourneyTotalsActivity extends Activity {
         m_mode = m_app.getappMode();
 
         List<Tournament> allTourneys = TourneyManagerDao.GetAllTournaments(TourneyTotalsActivity.this);
+        Collections.sort(allTourneys,new TotalsCompare());
         int refundedVal = 0;
         int houseCutVal = 0;
-        ArrayList<String> tourneyIds = new ArrayList<>();
-
+        tourneyIds.clear();
 
         for(Tournament t : allTourneys){
-            tourneyIds.add(Integer.toString(t.getId()));
             if(t.isEndedEarly()){
                 refundedVal += t.getInfo().getEntryPrice() * t.getInfo().getNumberOfEntrants();
+                String s = "Tournament #: " + String.valueOf(t.getId()) + " - Refunded";
+                tourneyIds.add(s);
             }
             else if(!t.isEndedEarly() && !t.isRunning()){
                 houseCutVal += t.getInfo().getHouseCut();
+                String s = "Tournament #: " + String.valueOf(t.getId()) + " - $" + String.valueOf(t.getInfo().getHouseCut());
+                tourneyIds.add(s);
             }
         }
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tourneyIds);
-        tourneyList.setAdapter(adapter);
         numTourneys.setText(Integer.toString(allTourneys.size()));
         refunded.setText("$"+Integer.toString(refundedVal)+".00");
         profit.setText("$"+Integer.toString(houseCutVal)+".00");
-
+        adapter.notifyDataSetChanged();
 
     }
 
@@ -71,10 +87,13 @@ public class TourneyTotalsActivity extends Activity {
                 TextView item = (TextView)view;
                 if(!item.getText().toString().isEmpty())
                 {
+                    String s = item.getText().toString();
+                    String num = s.substring(13,s.indexOf('-'));
+                    num = num.replaceAll(" ","");
                     Intent i = new Intent(TourneyTotalsActivity.this,CreateTourneyActivity.class);
                     Bundle extras = new Bundle();
                     extras.putBoolean("viewTourneyInfo",true);
-                    extras.putString("tourneyId",item.getText().toString());
+                    extras.putString("tourneyId",num);
                     i.putExtras(extras);
                     startActivity(i);
                 }
@@ -92,4 +111,6 @@ public class TourneyTotalsActivity extends Activity {
     private EditText numTourneys;
     private EditText profit;
     private EditText refunded;
+    private ArrayList<String> tourneyIds;
+    private ArrayAdapter<String> adapter;
 }
