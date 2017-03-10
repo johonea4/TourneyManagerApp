19b397757c6285a1233dc11df6f6f0e5e1041b50
Rajendra Ugrani <rgugrani@us.ibm.com>
2017-03-10 18:49:46 -0500

    MatchDBTest

---
+++ GroupProject/TourneyManager/app/src/test/java/edu/gatech/seclass/tourneymanager/MatchDBTest.java
@@ -0,0 +1,42 @@
+package edu.gatech.seclass.tourneymanager;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import edu.gatech.seclass.tourneymanager.db.MatchDBHelper;
+import edu.gatech.seclass.tourneymanager.models.Match;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class MatchDBTest {
+
+    @Mock
+    Context fakeContext;
+
+    @Test
+    public void test_add_match() throws Exception {
+
+        /*MatchDBHelper matchDBHelper = new MatchDBHelper(fakeContext);
+        Match match = new Match();
+        match.setRoundId(1);
+        match.setFinished(true);
+        match.setRunning(false);
+        match.setPlayer1("test1");
+        match.setPlayer2("test2");
+        match.setId(1);
+        match.setTournamentId(1);
+
+        matchDBHelper.addMatch(match);*/
+
+    }
+
+}
